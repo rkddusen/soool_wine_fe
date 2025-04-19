f239e07cb693755a2a9da5676db85ddb82789f4c
rkddusen <k99endus@naver.com>
2025-04-19 11:27:12 +0900

    문구 스타일 변경

--- src/components/main/SearchArea.tsx
+++ src/components/main/SearchArea.tsx
@@ -3,12 +3,12 @@ import SearchBar from "../common/SearchBar";
 const SearchArea = () => {
   return (
     <section className="flex flex-col items-center justify-center w-full">
-      <div className="text-center md:text-84 sm:text-64 text-54 font-display text-(--main) md:py-70 sm:py-50 py-30">
+      <div className="text-center md:text-84 sm:text-64 text-48 font-display text-(--main) sm:pt-70 sm:pb-50 pt-50 pb-20">
         <p>What a Wonderful</p>
         <p>Wine</p>
       </div>
-      <div className="flex flex-row items-center w-full h-60 max-w-500">
-        <div className="w-full h-full border rounded-30 border-(--main)">
+      <div className="flex flex-row items-center w-full h-55 max-w-500">
+        <div className="w-full h-full rounded-30 border border-(--main)">
           <SearchBar />
         </div>
       </div>
