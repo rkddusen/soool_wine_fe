f085d645effe9de70706f70ef5a20770ab06a036
rkddusen <k99endus@naver.com>
2025-06-03 23:11:02 +0900

    와인 getWine react-query 수정

--- src/components/wine/WineSection.tsx
+++ src/components/wine/WineSection.tsx
@@ -1,72 +1,36 @@
-import { useEffect, useState } from "react";
-import { useMutation } from "@tanstack/react-query";
-import { getWine } from "@/utils/api";
-import { WineResponse } from "@/models/Api";
 import { WineWithWinery } from "@/models/Wine";
-import { useParams } from "react-router-dom";
 import {
   WineCityAndWineryBox,
   WineImageBox,
   WineNameBox,
   WineTypeBox,
   WineCountryBox,
+  WineInteractionBox,
 } from "./WineInfoBox";
 
-const WineSection = () => {
-  const { id } = useParams();
-  const [wineInfo, setWineInfo] = useState<WineWithWinery | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+interface WineSectionProps {
+  wineInfo: WineWithWinery;
+}
 
-  const callGetWine = async (id: number): Promise<WineResponse> => {
-    const response: WineResponse = await getWine(id);
-    return response;
-  };
-
-  const mutation = useMutation<WineResponse, Error, number>({
-    mutationFn: callGetWine,
-    onMutate: () => {
-      setLoading(true);
-    },
-    onSuccess: ({ content }: WineResponse) => {
-      console.log(content);
-      setWineInfo(content);
-    },
-    onError: (error: Error) => {
-      setError("Error get wine");
-      console.log("Error get wine: ", error);
-    },
-    onSettled: () => {
-      setLoading(false);
-    },
-  });
-
-  useEffect(() => {
-    mutation.mutate(Number(id));
-  }, []);
-
-  if (loading) return <div>Loading</div>;
-  if (!wineInfo) return <div></div>;
+const WineSection = ({ wineInfo }: WineSectionProps) => {
   return (
-    <div>
-      <div className="flex flex-col justify-center gap-20 px-20 pt-20 mx-auto md:flex-row md:px-40 md:max-w-1000 max-w-500">
-        <WineImageBox image={wineInfo.image} ename={wineInfo.ename} />
-        <div className="flex flex-col w-full gap-20">
-          <WineTypeBox type={wineInfo.type} />
-          <WineCountryBox country={wineInfo.country} />
-          <WineCityAndWineryBox
-            region={wineInfo.region}
-            city={wineInfo.city}
-            winery={wineInfo.winery}
-          />
-          <WineNameBox
-            ename={wineInfo.ename}
-            kname={wineInfo.kname}
-            abv={wineInfo.abv}
-          />
-        </div>
+    <div className="flex flex-col justify-center gap-20 px-20 mx-auto mt-20 md:flex-row md:px-40 md:max-w-1000 max-w-500">
+      <WineImageBox image={wineInfo.image} ename={wineInfo.ename} />
+      <div className="flex flex-col w-full gap-20">
+        <WineTypeBox type={wineInfo.type} />
+        <WineCountryBox country={wineInfo.country} />
+        <WineCityAndWineryBox
+          region={wineInfo.region}
+          city={wineInfo.city}
+          winery={wineInfo.winery}
+        />
+        <WineNameBox
+          ename={wineInfo.ename}
+          kname={wineInfo.kname}
+          abv={wineInfo.abv}
+        />
+        <WineInteractionBox wineId={wineInfo.id} />
       </div>
-      <div className="px-20 mx-auto md:px-40 max-w-1280"></div>
     </div>
   );
 };
--- src/pages/WinePage.tsx
+++ src/pages/WinePage.tsx
@@ -1,9 +1,30 @@
 import WineSection from "@/components/wine/WineSection";
+import { WineResponse } from "@/models/Api";
+import { getWine } from "@/utils/api";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
 
 const WinePage = () => {
+  const { id } = useParams();
+  const wineId = Number(id);
+
+  const callGetWine = async (id: number): Promise<WineResponse> => {
+    const response: WineResponse = await getWine(id);
+    return response;
+  };
+
+  const { data, isError, isLoading } = useQuery<WineResponse>({
+    queryKey: ["wineInfo", wineId],
+    queryFn: () => callGetWine(wineId),
+    enabled: !isNaN(wineId),
+  });
+
+  if (isLoading) return <div>로딩 중입니다.</div>;
+  if (isError) return <div>데이터 요청에 실패했습니다.</div>;
+  if (!data?.content) return <div>와인 정보가 없습니다.</div>;
   return (
     <>
-      <WineSection />
+      <WineSection wineInfo={data.content} />
     </>
   );
 };
