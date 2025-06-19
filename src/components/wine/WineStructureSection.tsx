e6e274b08742494feae458d217ffc83197f2b91a
rkddusen <k99endus@naver.com>
2025-06-19 14:32:37 +0900

    와인 구조 차트뷰 디자인 수정 및 타입 방어

--- src/components/wine/WineStructureSection.tsx
+++ src/components/wine/WineStructureSection.tsx
@@ -8,10 +8,14 @@ interface StructureProps {
 const WineStructureSection = ({ structure }: StructureProps) => {
   return (
     <div className="flex flex-col justify-center gap-20 px-20 mx-auto mt-20 md:flex-row md:px-40 md:max-w-1000 max-w-500">
-      <div className="flex items-center justify-center w-full bg-white rounded-15">
-        <ChartView structure={structure} />
+      <div className="flex items-center justify-center w-full bg-white min-h-300 rounded-15">
+        {structure ? (
+          <ChartView structure={structure} />
+        ) : (
+          <p>와인 정보를 불러오는 데 오류가 발생했습니다.</p>
+        )}
       </div>
-      <div className="flex items-center justify-center w-full bg-white rounded-15">
+      <div className="flex items-center justify-center w-full bg-white min-h-300 rounded-15">
         <DetailView structure={structure} />
       </div>
     </div>
@@ -22,35 +26,38 @@ export default WineStructureSection;
 
 const ChartView = ({ structure }: StructureProps) => {
   return (
-    <div className="p-40 grid grid-cols-[min-content_min-content_min-content] justify-center gap-y-2">
+    <div className="p-40 grid grid-cols-[min-content_min-content_min-content] justify-center gap-2">
       <div className="text-right">
-        <span className="text-nowrap text-14 sm:text-16">당도</span>
+        <span className="text-nowrap text-14 md:text-16">당도</span>
       </div>
       <div />
       <div>
-        <span className="text-nowrap text-14 sm:text-16">산도</span>
+        <span className="text-nowrap text-14 md:text-16">산도</span>
       </div>
 
       <div />
-      <div className="flex">
-        <ChartViewDegree degree={structure.sweetness} label="sweetness" />
-        <ChartViewDegree degree={structure.acidity} label="acidity" />
+      <div className="flex gap-2">
+        <ChartViewDegree
+          degree={structure.sweetness ?? null}
+          label="sweetness"
+        />
+        <ChartViewDegree degree={structure.acidity ?? null} label="acidity" />
       </div>
       <div />
 
       <div />
-      <div className="flex">
-        <ChartViewDegree degree={structure.body} label="body" />
-        <ChartViewDegree degree={structure.tannin} label="tannin" />
+      <div className="flex gap-2">
+        <ChartViewDegree degree={structure.body ?? null} label="body" />
+        <ChartViewDegree degree={structure.tannin ?? null} label="tannin" />
       </div>
       <div />
 
       <div className="text-right">
-        <span className="text-nowrap text-14 sm:text-16">바디</span>
+        <span className="text-nowrap text-14 md:text-16">바디</span>
       </div>
       <div />
       <div>
-        <span className="text-nowrap text-14 sm:text-16">타닌</span>
+        <span className="text-nowrap text-14 md:text-16">타닌</span>
       </div>
     </div>
   );
@@ -63,10 +70,10 @@ const ChartViewDegree = ({ degree, label }: ChartViewDegreeProps) => {
   return (
     <>
       <div
-        className={`shrink-0 relative overflow-hidden w-55 h-55 sm:w-75 sm:h-75 ${WINESTRUCTURE[label].rotation}`}
+        className={`shrink-0 relative overflow-hidden w-55 h-55 md:w-75 md:h-75 ${WINESTRUCTURE[label].rotation}`}
       >
         <div
-          className={`absolute rounded-full bg-(--gray-f5) w-110 h-110 -right-55 -bottom-55 sm:w-150 sm:h-150 sm:-right-75 sm:-bottom-75 `}
+          className={`absolute rounded-full bg-(--gray-f5) w-110 h-110 -right-55 -bottom-55 md:w-150 md:h-150 md:-right-75 md:-bottom-75 `}
         ></div>
         <div className="absolute top-0 left-0 w-full h-full overflow-hidden">
           <div
@@ -75,7 +82,7 @@ const ChartViewDegree = ({ degree, label }: ChartViewDegreeProps) => {
               degree && degree >= 5 ? "block" : "hidden"
             }
               w-110 h-110 -right-55 -bottom-55
-              sm:w-150 sm:h-150 sm:-right-75 sm:-bottom-75
+              md:w-150 md:h-150 md:-right-75 md:-bottom-75
             `}
           ></div>
           <div
@@ -84,7 +91,7 @@ const ChartViewDegree = ({ degree, label }: ChartViewDegreeProps) => {
               degree && degree >= 4 ? "block" : "hidden"
             }
               w-90 h-90 -right-45 -bottom-45
-              sm:w-120 sm:h-120 sm:-right-60 sm:-bottom-60
+              md:w-120 md:h-120 md:-right-60 md:-bottom-60
             `}
           ></div>
           <div
@@ -93,7 +100,7 @@ const ChartViewDegree = ({ degree, label }: ChartViewDegreeProps) => {
               degree && degree >= 3 ? "block" : "hidden"
             }
               w-70 h-70 -right-35 -bottom-35
-              sm:w-90 sm:h-90 sm:-right-45 sm:-bottom-45
+              md:w-90 md:h-90 md:-right-45 md:-bottom-45
             `}
           ></div>
           <div
@@ -102,7 +109,7 @@ const ChartViewDegree = ({ degree, label }: ChartViewDegreeProps) => {
               degree && degree >= 2 ? "block" : "hidden"
             }
               w-50 h-50 -right-25 -bottom-25
-              sm:w-60 sm:h-60 sm:-right-30 sm:-bottom-30
+              md:w-60 md:h-60 md:-right-30 md:-bottom-30
             `}
           ></div>
           <div
@@ -111,7 +118,7 @@ const ChartViewDegree = ({ degree, label }: ChartViewDegreeProps) => {
               degree && degree >= 1 ? "block" : "hidden"
             }
               w-30 h-30 -right-15 -bottom-15
-              sm:w-30 sm:h-30 sm:-right-15 sm:-bottom-15
+              md:w-30 md:h-30 md:-right-15 md:-bottom-15
             `}
           ></div>
         </div>
