5981d0da474840c1d051fca14115e4c05c41137c
rkddusen <k99endus@naver.com>
2025-09-30 17:44:08 +0900

    [내메모]스켈레톤UI 적용

--- src/features/auth/my/components/MyMemo/index.tsx
+++ src/features/auth/my/components/MyMemo/index.tsx
@@ -4,8 +4,7 @@ import MyMemoItem from "./MyMemoItem";
 import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
 import { useDeleteMemo } from "@/hooks/memo/useDeleteMemo";
 import { getMyMemos } from "../../api";
-import LoadingFind from "@/assets/LoadingFind.svg?react";
-import LoadingCircle from "@/assets/LoadingCircle.svg?react";
+import MyMemoItemSkeleton from "./MyMemoItemSkeleton";
 
 const MyMemo = () => {
   const queryKey = ["my-memos"];
@@ -26,13 +25,13 @@ const MyMemo = () => {
       <p className="text-16 md:text-20 flex items-center gap-10">
         <span className="text-24 md:text-32 font-medium">내 메모</span>
       </p>
-      <div>
+      <div className="mt-20">
         {!isError ? (
           <>
             {!isLoading ? (
               <>
                 {allMemo.length ? (
-                  <ul className="mt-20">
+                  <ul>
                     {allMemo?.map((memoData) => (
                       <MyMemoItem
                         key={memoData.clientId}
@@ -45,28 +44,31 @@ const MyMemo = () => {
                   <p>작성한 메모가 없어요.</p>
                 )}
                 {hasNextPage && (
-                  <div className="text-center mt-50">
+                  <div className="text-center">
                     {!isFetchingNextPage ? (
                       <button
                         onClick={() => fetchNextPage()}
                         disabled={isLoading}
-                        className="inline-block border rounded-full border-(--gray-78) hover:cursor-pointer"
+                        className="inline-block border rounded-full border-(--gray-78) hover:cursor-pointer mt-50"
                       >
                         <div className="py-10 px-30 text-12">더보기</div>
                       </button>
                     ) : (
-                      <div className="flex flex-col items-center mx-auto">
-                        <LoadingCircle />
-                      </div>
+                      <ul>
+                        {Array.from({ length: 10 }).map((_, i) => (
+                          <MyMemoItemSkeleton key={i} />
+                        ))}
+                      </ul>
                     )}
                   </div>
                 )}
               </>
             ) : (
-              <div className="flex flex-col items-center gap-10 mx-auto py-100">
-                <LoadingFind />
-                <p className="text-(--gray-78) text-18">메모 가져오는 중...</p>
-              </div>
+              <ul>
+                {Array.from({ length: 10 }).map((_, i) => (
+                  <MyMemoItemSkeleton key={i} />
+                ))}
+              </ul>
             )}
           </>
         ) : (
