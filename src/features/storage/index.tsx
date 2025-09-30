71e75d153df21b6c5f1f7968b863589e693d424d
rkddusen <k99endus@naver.com>
2025-09-30 09:35:02 +0900

    [와인창고]스켈레톤UI 적용

--- src/features/storage/index.tsx
+++ src/features/storage/index.tsx
@@ -1,14 +1,13 @@
 // features/Storage/index.tsx
 // 와인창고 페이지
 import NoResultsFound from "@/assets/NoResultsFound.svg?react";
-import LoadingFind from "@/assets/LoadingFind.svg?react";
-import LoadingCircle from "@/assets/LoadingCircle.svg?react";
 import { SearchArea, FilterSearch, WineItem } from "./components";
 import { useSearchQuery } from "./hooks/useSearchQuery";
 import { useFilterQuery } from "./hooks/useFilterQuery";
 import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
 import { getWines } from "./api";
 import { WineWithWinery } from "@/models/Wine";
+import WineItemSkeleton from "./components/WineItemSkeleton";
 
 const Storage = () => {
   const { search } = useSearchQuery();
@@ -38,11 +37,11 @@ const Storage = () => {
         <div className="px-20 mx-auto mt-20 md:px-40 max-w-1280">
           {!isLoading ? (
             <>
-              <div className="flex items-center px-10 h-60">
+              {/* <div className="flex items-center px-10 h-60">
                 <p className="text-14 text-(--gray-78) font-medium">
-                  {/* {totalElements} Wines */}
+                  {totalElements} Wines
                 </p>
-              </div>
+              </div> */}
               <div className="flex flex-wrap justify-center gap-40">
                 {wines.length > 0 ? (
                   wines.map((w, i) => (
@@ -68,17 +67,20 @@ const Storage = () => {
                       <div className="py-10 px-30 text-12">더보기</div>
                     </button>
                   ) : (
-                    <div className="flex flex-col items-center mx-auto">
-                      <LoadingCircle />
+                    <div className="flex flex-wrap justify-center gap-40">
+                      {Array.from({ length: 10 }).map((_, i) => (
+                        <WineItemSkeleton key={i} />
+                      ))}
                     </div>
                   )}
                 </div>
               )}
             </>
           ) : (
-            <div className="flex flex-col items-center gap-10 mx-auto py-100">
-              <LoadingFind />
-              <p className="text-(--gray-78) text-18">와인 가져오는 중...</p>
+            <div className="flex flex-wrap justify-center gap-40">
+              {Array.from({ length: 10 }).map((_, i) => (
+                <WineItemSkeleton key={i} />
+              ))}
             </div>
           )}
         </div>
